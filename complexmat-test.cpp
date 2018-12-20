73df8bac9e06ce9257cb88595e7cc96124626388
Michal Sojka <michal.sojka@cvut.cz>
2018-12-20 09:57:15 +0100

    Simplify the test even more

--- complexmat-test.cpp
+++ complexmat-test.cpp
@@ -8,11 +8,11 @@ int main(int argc, char *argv[])
     ComplexMat_<32, 32, 44, 1> a(32, 32, 44, 1), b(32, 32, 44, 1), c(32, 32, 44, 1);
     ComplexMat_<32, 32, 1, 1> x(32, 32, 1, 1), y(32, 32, 1, 1);
 
-    a = b + c;
+    b + c;
 
-    b = a.mul(y);
+//     b = a.mul(y);
 
-    cout << a << endl;
+//     cout << a << endl;
 
     return 0;
 }
